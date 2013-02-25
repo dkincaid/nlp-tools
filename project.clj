51ecf92f4195470c63de34d3ae2036e760d1046b
Dave Kincaid <kincaid.dave@gmail.com>
2013-02-24 21:23:03 -0600

    Updating project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,11 @@
 (defproject nlp-tools "0.0.1-SNAPSHOT"
   :description "A set of Clojure NLP tools made easy to use."
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
+  :min-lein-version "2.0.0"
+  :warn-on-reflection true
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.lucene/lucene-core "3.5.0"]
                  [org.apache.lucene/lucene-spellchecker "3.5.0"]
